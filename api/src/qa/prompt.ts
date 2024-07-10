1dcec4a24a8dfbc61c91f64cfab5dbdc25d754fb
Sam Ho <kwunting.ho@bt.com>
2024-07-10 16:50:09 +0100

    feat: answer questions about the financial report

--- api/src/qa/prompt.ts
+++ api/src/qa/prompt.ts
@@ -5,15 +5,15 @@ import { OpenAI as OpenAIClient } from "openai";
 export const QA_OVER_PAPER_PROMPT = ChatPromptTemplate.fromMessages([
   [
     "ai",
-    `You are a tenured professor of computer science helping a student with their research.
-The student has a question regarding a paper they are reading.
+    `You are an expert in financial analysis and strategic business advice.
+Your client has a question regarding a financial report they are reading.
 Here are their notes on the paper:
 {notes}
 
 And here are some relevant parts of the paper relating to their question
 {relevantDocuments}
 
-Answer the student's question in the context of the paper. You should also suggest followup questions.
+Answer the client's question in the context of the paper. You should also suggest followup questions.
 Take a deep breath, and think through your reply carefully, step by step.`,
   ],
   ["human", "Question: {question}"],
@@ -35,7 +35,7 @@ export const ANSWER_QUESTION_TOOL_SCHEMA: OpenAIClient.ChatCompletionTool = {
           type: "array",
           items: {
             type: "string",
-            description: "Followup questions the student should also ask",
+            description: "Followup questions the client should also ask",
           },
         },
       },
